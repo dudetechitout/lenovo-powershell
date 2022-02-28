61784fb299407cff8c213bf5a3c0a1d30fdf8242
dudetechitout <100632668+dudetechitout@users.noreply.github.com>
2022-02-28 16:31:48 -0500

    Update auto-keyboard-backlight.ps1

--- auto-keyboard-backlight/auto-keyboard-backlight.ps1
+++ auto-keyboard-backlight/auto-keyboard-backlight.ps1
@@ -1,14 +1,40 @@
-# Settings
-[string]$path = 'C:\ProgramData\Lenovo\ImController\Plugins\ThinkKeyboardPlugin\x86\';
-[int]$level = 2; # Backlight level: 0 - Off, 1 - Dim, 2 - On
+try {
 
-# Don't modify below
-[string]$core = $path + 'Keyboard_Core.dll';
-[string]$contract = $path + 'Contract_Keyboard.dll';
+	[string]$cur_user = $env:UserName
+  
+	if (!(Test-Path "C:\Users\$cur_user\Documents\scripts\storage.ini")) {
+	   New-Item -Path "C:\Users\$cur_user\Documents\scripts\storage.ini" -ItemType File
+	}
 
-Add-Type -Path $core;
-Add-Type -Path $contract;
+	[string]$path = "C:\ProgramData\Lenovo\ImController\Plugins\ThinkKeyboardPlugin\x86\";
 
-[Keyboard_Core.KeyboardControl]$control =  New-Object -TypeName  'Keyboard_Core.KeyboardControl';
+	[string]$file_data = Get-Content "C:\Users\$cur_user\Documents\scripts\storage.ini"
 
-$control.SetKeyboardBackLightStatus($level, $null);
+	if((Get-CimInstance win32_KEYBOARD | Select-String -Pattern "USB").length -ge 2) {
+		[int]$level = 0; # Backlight level: 0 - Off, 1 - Dim, 2 - On
+		Set-Content -Path "C:\Users\$cur_user\Documents\scripts\storage.ini" -Value 0
+	} else {
+		[int]$level = 2; # Backlight level: 0 - Off, 1 - Dim, 2 - On
+		Set-Content -Path "C:\Users\$cur_user\Documents\scripts\storage.ini" -Value 2
+	}
+
+	[string]$grab_param = $args[0]
+
+	if($grab_param) {
+		[int]$level = 0;
+		Set-Content -Path "C:\Users\$cur_user\Documents\scripts\storage.ini" -Value 0
+	}
+
+	if($level -ne $file_data -Or $grab_param) {
+		# Don't modify below
+		[string]$core = $path + 'Keyboard_Core.dll';
+		[string]$contract = $path + 'Contract_Keyboard.dll';
+
+		Add-Type -Path $core;
+		Add-Type -Path $contract;
+
+		[Keyboard_Core.KeyboardControl]$control =  New-Object -TypeName  'Keyboard_Core.KeyboardControl';
+
+		$control.SetKeyboardBackLightStatus($level, $null);
+	}
+} catch { }
